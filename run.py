8e73ddf5158fea56a4064a6a0fd175e121a6e1a8
yqeslcswjttzbjszh <142977085+yqeslcswjttzbjszh@users.noreply.github.com>
2023-08-24 22:08:26 +0800

    Create run.py

---
+++ run.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+
+class NeRF(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        return x
+
+
+if __name__ == "__main__":
+    model = NeRF()
